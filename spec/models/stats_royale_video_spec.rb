70214b0056c4b09a235e4a8f86bdfba38a9903d7
Yusuke Doi <yuusukedoi23@gmail.com>
2022-02-28 21:27:09 +0900

    動画削除のテストを追加する

---
+++ spec/models/stats_royale_video_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe StatsRoyaleVideo, type: :model do
+  describe '.old' do
+    subject { described_class.old }
+
+    let!(:older_video) { create(:stats_royale_video, published_at: 1.week.ago) }
+    let!(:newer_video) { create(:stats_royale_video, published_at: Time.current.ago(6.days + 23.hours)) }
+
+    it "一週間前のレコードが含まれること" do
+      expect(subject).to include older_video
+    end
+
+    it "一週間以内のレコードは含まれないこと" do
+      expect(subject).not_to include newer_video
+    end
+  end
+end
